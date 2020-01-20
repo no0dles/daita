01553ae5767b14c09804b4c352bfc7a40b738936
Pascal Bertschi <pascal@bertschi.io>
2020-01-20 21:59:42 +0100

    remove migration from testcase

--- packages/test/test/schema/src/schema.ts
+++ packages/test/test/schema/src/schema.ts
@@ -1,10 +1,8 @@
 import {RelationalSchema} from '@daita/core';
 import { User } from "./models/user";
-import {FooMigration} from './src/migrations/202001212039-foo';
 
 const schema = new RelationalSchema();
 
 schema.table(User, {key: ['username'] });
-schema.migration(FooMigration);
 
 export = schema;
