32943373345501580cb015cfb0c03e218e37f07c
Pascal Bertschi <pascal@bertschi.io>
2020-10-13 17:58:42 +0200

    publish alpha.5

--- src/containers/http/http-sqlite/index.ts
+++ src/containers/http/http-sqlite/index.ts
@@ -1,9 +1,21 @@
 import { run } from '../server';
 import { sqliteAdapter } from '../../../packages/sqlite-adapter/sqlite-adapter-implementation';
 import { getClient } from '../../../packages/relational/client';
+import { OrmRuleContext } from '../../../packages/orm/context';
+import { Rule } from '../../../packages/relational/permission/description';
 
 const client = getClient(sqliteAdapter);
-run(client).catch((err) => {
+const ruleContext = new OrmRuleContext(client);
+const rules: Rule[] = [];
+
+async function reloadRules() {
+  const newRules = await ruleContext.getRules();
+  rules.splice(0, rules.length);
+  rules.push(...newRules);
+}
+
+reloadRules();
+run(client, rules).catch((err) => {
   console.error(err);
   process.exit(1);
 });
