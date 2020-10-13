601f859ddaf92b68d2d2ebfaf7135cfc2105bfbf
Pascal Bertschi <pascal@bertschi.io>
2020-10-13 17:50:00 +0200

    add http adapter

--- src/packages/http-adapter/index.ts
+++ src/packages/http-adapter/index.ts
@@ -1,2 +1,6 @@
+import { httpAdapter } from './adapter-implementation';
+
 export { HttpTransactionAdapter } from './http-transaction-adapter';
 export { HttpDataAdapter } from './http-data-adapter';
+
+export const adapter = httpAdapter;
--- src/scripts/shell.ts
+++ src/scripts/shell.ts
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 
 export function shell(cmd: string, args: string[], cwd: string) {
-  return new Promise((resolve, reject) => {
+  return new Promise<void>((resolve, reject) => {
     const ps = spawn(cmd, args, {
       cwd,
       stdio: [process.stdin, process.stdout, process.stderr],
