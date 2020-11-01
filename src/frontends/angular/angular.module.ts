9c2520f1c5f0ab7d0cd05105e15f4b9a0eb157ab
Pascal Bertschi <pascal@bertschi.io>
2020-11-01 21:02:49 +0100

    added angular lib

---
+++ src/frontends/angular/angular.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { AngularService } from './angular.service';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [AngularService],
+  exports: [],
+})
+export class AngularModule {}
